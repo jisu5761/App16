9b96832fe63dfb81f96f4d8b39c7fae1a37d5a56
jisu5761 <jisu576137@gmail.com>
2020-01-30 09:51:51 +0900

    초기 커밋.

---
+++ App16/App16/BluetoothDataPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App16
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BluetoothDataPage : ContentPage
+    {
+        private readonly IDevice _connectedDevice;
+        public BluetoothDataPage(IDevice connectedDevice)
+        {
+            InitializeComponent();
+            _connectedDevice = connectedDevice;
+        }
+
+
+
+        private async void SendMessageButton_Clicked(object sender, EventArgs e)
+        { 
+            try
+            {
+                var service = await _connectedDevice.GetServiceAsync(GattCharacteristicIdentifiers_WOOIN.ServiceId);
+                if (service != null)
+                {
+                    var characteristic = await service.GetCharacteristicAsync(GattCharacteristicIdentifiers_WOOIN.WriteData);
+                    if (characteristic != null)
+                    {
+                        byte[] senddata = Encoding.UTF8.GetBytes(string.IsNullOrEmpty(SendMessageLabel.Text) ? "jenx.si was here" : SendMessageLabel.Text);
+                        var bytes = await characteristic.WriteAsync(senddata);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Notice", ex.Message.ToString(), "Error!");
+
+            }
+        }
+
+        private async void GetManufacturerDataButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var service = await _connectedDevice.GetServiceAsync(GattCharacteristicIdentifiers_WOOIN.ServiceId);
+                if (service != null)
+                {
+                    var characteristic = await service.GetCharacteristicAsync(GattCharacteristicIdentifiers_WOOIN.ReadData);
+                    if (characteristic != null)
+                    {
+                        var bytes = await characteristic.ReadAsync();
+                        var str = Encoding.UTF8.GetString(bytes);
+                        ManufacturerLabel.Text = str;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Notice", ex.Message.ToString(), "Error !!");
+            }
+        }
+    }
+}
---
+++ App16/App16/GattCharacteristicIdentifiers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App16
+{
+    public class GattCharacteristicIdentifiers
+    {
+        public static Guid ServiceId = Guid.Parse("19536E67-3682-4588-9F3A-5340B6712150");
+
+        // read only characteristic
+        public static Guid FirmwareVersion = Guid.Parse("BC1926EA-6FFA-4D04-928B-76CCCD068CEA");
+
+        // read-write characteristic -> exchange data
+        public static Guid DataExchange = Guid.Parse("72563044-DB33-4692-A45D-C5212EEBABFA");
+
+        // write characteristic -> exchange data
+        public static Guid InitData = Guid.Parse("168952DE-C604-4401-881E-989996888363");
+
+        // use predefined Bluetooth SIG definitions for well know characteristics.
+        public static Guid ManufacturerName = Guid.Parse("00002a29-0000-1000-8000-00805f9b34fb");
+    }
+
+    public class GattCharacteristicIdentifiers_WOOIN
+    {
+        public static Guid ServiceId = Guid.Parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+
+        // read only characteristic
+        public static Guid ReadData = Guid.Parse("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+
+        // write characteristic -> exchange data
+        public static Guid WriteData = Guid.Parse("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+
+    }
+}
